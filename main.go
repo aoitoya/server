61b3b9dae9500877118ad5d4dfde1917f3f182a9
Saptarshi <96932308+aoitoya@users.noreply.github.com>
2026-05-04 11:04:20 +0530

    you get what you give

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
 	"log"
 	"net"
 )
@@ -25,12 +28,80 @@ func main() {
 func handle(conn net.Conn) {
 	defer conn.Close()
 
+	req, err := parseReq(conn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	log.Println(req.Method, req.Path)
+
+	sendRes(conn, req)
+}
+
+type HTTPReq struct {
+	Method   string
+	Path     string
+	Protocol string
+	Headers  map[string]string
+}
+
+func parseReq(conn net.Conn) (*HTTPReq, error) {
+	reader := bufio.NewReader(conn)
+
+	req := HTTPReq{Headers: make(map[string]string)}
+
+	n := 0
+
+	for {
+		line, err := reader.ReadBytes('\n')
+
+		if err != nil {
+			return nil, err
+		}
+
+		line = bytes.TrimSuffix(line, []byte("\r\n"))
+
+		if len(line) == 0 {
+			break
+		}
+
+		if n == 0 {
+			b := bytes.Split(line, []byte{' '})
+
+			if len(b) != 3 {
+				return nil, errors.New("Invalid request")
+			}
+
+			req.Method = string(b[0])
+			req.Path = string(b[1])
+			req.Protocol = string(b[2])
+		} else {
+			b := bytes.Split(line, []byte(": "))
+			if len(b) != 2 {
+				return nil, errors.New("Corrupt headers")
+			}
+
+			key := string(b[0])
+			val := string(b[1])
+
+			req.Headers[key] = val
+
+		}
+
+		n++
+	}
+
+	return &req, nil
+}
+
+func sendRes(conn net.Conn, req *HTTPReq) {
 	writer := bufio.NewWriter(conn)
 
 	writer.WriteString("HTTP/1.1 200 OK\r\n")
-	writer.WriteString("Content-Type: text/plain\r\n")
-	writer.WriteString("Connection: close\r\n")
 	writer.WriteString("\r\n")
-	writer.WriteString("Hello\n")
+	for k, v := range req.Headers {
+		fmt.Fprintf(writer, "%s: %s\r\n", k, v)
+	}
 	writer.Flush()
 }
