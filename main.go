6005cd38522f9fb190fb3bf8d5929245e5867485
Saptarshi <96932308+aoitoya@users.noreply.github.com>
2026-05-05 13:53:16 +0530

    you get what you give with body

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -28,13 +31,17 @@ func main() {
 func handle(conn net.Conn) {
 	defer conn.Close()
 
-	req, err := parseReq(conn)
+	reader := bufio.NewReader(conn)
+	req, err := parseReq(reader)
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	log.Println(req.Method, req.Path)
+
+	parseBody(reader, req)
 
 	sendRes(conn, req)
 }
@@ -44,11 +51,10 @@ type HTTPReq struct {
 	Path     string
 	Protocol string
 	Headers  map[string]string
+	Body     []byte
 }
 
-func parseReq(conn net.Conn) (*HTTPReq, error) {
-	reader := bufio.NewReader(conn)
-
+func parseReq(reader *bufio.Reader) (*HTTPReq, error) {
 	req := HTTPReq{Headers: make(map[string]string)}
 
 	n := 0
@@ -67,22 +73,22 @@ func parseReq(conn net.Conn) (*HTTPReq, error) {
 		}
 
 		if n == 0 {
-			b := bytes.Split(line, []byte{' '})
+			parts := strings.Fields(string(line))
 
-			if len(b) != 3 {
+			if len(parts) != 3 {
 				return nil, errors.New("Invalid request")
 			}
 
-			req.Method = string(b[0])
-			req.Path = string(b[1])
-			req.Protocol = string(b[2])
+			req.Method = parts[0]
+			req.Path = parts[1]
+			req.Protocol = parts[2]
 		} else {
-			b := bytes.Split(line, []byte(": "))
+			b := bytes.SplitN(line, []byte(": "), 2)
 			if len(b) != 2 {
 				return nil, errors.New("Corrupt headers")
 			}
 
-			key := string(b[0])
+			key := strings.ToLower(string(bytes.TrimSpace(b[0])))
 			val := string(b[1])
 
 			req.Headers[key] = val
@@ -95,13 +101,42 @@ func parseReq(conn net.Conn) (*HTTPReq, error) {
 	return &req, nil
 }
 
+func parseBody(reader *bufio.Reader, req *HTTPReq) error {
+	cl, ok := req.Headers["content-length"]
+	if !ok {
+		return nil
+	}
+
+	length, err := strconv.Atoi(strings.TrimSpace(cl))
+
+	if err != nil {
+		fmt.Println("Invalid content length")
+	}
+
+	if length > 1<<24 {
+		return errors.New("Too long for me")
+	}
+
+	body := make([]byte, length)
+	_, err = io.ReadFull(reader, body)
+
+	if err != nil {
+		return err
+	}
+
+	req.Body = body
+	return nil
+}
+
 func sendRes(conn net.Conn, req *HTTPReq) {
 	writer := bufio.NewWriter(conn)
 
 	writer.WriteString("HTTP/1.1 200 OK\r\n")
-	writer.WriteString("\r\n")
 	for k, v := range req.Headers {
 		fmt.Fprintf(writer, "%s: %s\r\n", k, v)
 	}
+	writer.WriteString("\r\n")
+
+	writer.Write(req.Body)
 	writer.Flush()
 }
